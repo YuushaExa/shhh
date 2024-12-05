b1663c335bab60406b7ebf7a211f4a45aed6a400
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-12-06 01:41:42 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const axios = require('axios');
 
 module.exports = async (req, res) => {
     // Check if the request method is POST
@@ -13,25 +13,12 @@ module.exports = async (req, res) => {
     }
 
     try {
-        // Launch Puppeteer
-        const browser = await puppeteer.launch({
-            headless: true, // Run in headless mode
-            args: ['--no-sandbox', '--disable-setuid-sandbox'], // Required for Vercel
-        });
-        const page = await browser.newPage();
-
-        // Navigate to the provided URL
-        await page.goto(url, { waitUntil: 'networkidle2' });
-
-        // Get the content of the page
-        const content = await page.content();
-
-        // Close the browser
-        await browser.close();
-
-        // Send the content back to the client
+        // Fetch the HTML content using axios
+        const response = await axios.get(url);
+        
+        // Set the content type to text/html and send the response
         res.setHeader('Content-Type', 'text/html');
-        res.send(content);
+        res.send(response.data);
     } catch (error) {
         console.error('Error fetching the URL:', error);
         res.status(500).send('Error fetching the URL: ' + error.message);
